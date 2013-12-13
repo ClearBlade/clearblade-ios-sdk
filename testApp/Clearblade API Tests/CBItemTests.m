88c15be5a812a3b8c20bd9f7be5f9826ed0f3737
Tyler Dodge <tldodge@ncsu.edu>
2013-12-13 18:02:19 -0500

    Added More Unit Tests.
    All Classes now override description to provide more human friendly information about the object's state
    
    CBMessageClient
    @property isConnected - shows if the message client is connected
    @property host - shows the name of the host the message client is connected to
    -(void)connect - connects the message client to messagingAddress set in ClearBlade settings
    -(void)disconnect - disconnects from the current server
    
    CBQuery
    made properties query and OR private
    
    -(CBQuery *)startNextOrClause - allows you to construct queries with multiple or clauses
    
    ClearBlade
    +(instancetype)initSettingsWithAppKey:(NSString *)key
                            withAppSecret:(NSString *)secret
                        withServerAddress:(NSString *)address
                     withMessagingAddress:(NSString *)messagingAddress
    - ALlows you to initialize with a different URL for messaging platform.

---
+++ testApp/Clearblade API Tests/CBItemTests.m
@@ -0,0 +1,34 @@
+//
+//  CBItemTests.m
+//  testApp
+//
+//  Created by Tyler Dodge on 12/13/13.
+//  Copyright (c) 2013 ClearBlade. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CBItemTests : AsyncTestCase
+
+@end
+
+@implementation CBItemTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
---
+++ testApp/Clearblade API Tests/CBMessageClientTests.m
@@ -0,0 +1,34 @@
+//
+//  CBMessageClientTests.m
+//  testApp
+//
+//  Created by Tyler Dodge on 12/13/13.
+//  Copyright (c) 2013 ClearBlade. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CBMessageClientTests : AsyncTestCase
+
+@end
+
+@implementation CBMessageClientTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
